45594066254aaefd3af23bd6435921d76fbb07ef
tomashined <45771896+tomashined@users.noreply.github.com>
2020-04-05 17:17:14 +0300

    2-UnityEssential-5 (30 min in video)

---
+++ 2-UnityEssential-5/Assets/Scripts/NPC/HealthHelper.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthHelper : MonoBehaviour {
+
+	public int maxHealth = 100;
+	public int Health = 100;
+
+	public bool isDynamicHealthBarCreate = true;
+
+	UIHealthBarHelper _UIHealthBarHelper;
+
+	void Start () {
+		if (isDynamicHealthBarCreate)
+		{
+			GameObject healthBarSlider = Instantiate(Resources.Load("HealthBarSlider"), Vector3.zero, Quaternion.identity) as GameObject;
+			healthBarSlider.transform.SetParent(GameObject.Find("Canvas").transform);
+			_UIHealthBarHelper = healthBarSlider.GetComponent<UIHealthBarHelper>();
+			_UIHealthBarHelper.NPC = transform;
+		}
+	}
+	
+	void Update () {
+	
+	}
+}
