1bee88a114f403065412b8e30eadf6b8b73fb0aa
tomashined <45771896+tomashined@users.noreply.github.com>
2020-04-09 20:39:35 +0300

    2-UnityEssential-6 start

---
+++ 2-UnityEssential-6/Assets/Scripts/NPC/HealthHelper.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class HealthHelper : MonoBehaviour {
+
+	public int maxHealth = 100;
+	public int Health = 100;
+
+	public bool isDynamicHealthBarCreate = true;
+
+	UIHealthBarHelper _UIHealthBarHelper;
+
+	private bool Dead;
+
+	public bool MyProperty
+	{
+		get { return Dead; }
+		set { Dead = value; }
+	}
+
+	public int Kills { get; private set; }
+
+	void Start () {
+		if (isDynamicHealthBarCreate)
+		{
+			GameObject healthBarSlider = Instantiate(Resources.Load("HealthBarSlider"), Vector3.zero, Quaternion.identity) as GameObject;
+			healthBarSlider.transform.SetParent(GameObject.Find("Canvas").transform);
+			_UIHealthBarHelper = healthBarSlider.GetComponent<UIHealthBarHelper>();
+			_UIHealthBarHelper.NPC = transform;
+		}
+	}
+	
+	void Update () {
+	
+	}
+
+    public void GetDamage(int damage, HealthHelper killer)
+    {
+		if (Dead) return;
+
+		Health -= damage;
+
+		if (Health <= 0)
+		{
+			Dead = true;
+			killer.Kills += 1;
+			GetComponentInChildren<PlayerShooting>().Drop();
+			GetComponent<Animator>().SetBool("Dead", true);
+		}
+    }
+}
---
+++ 2-UnityEssential-6/Assets/Scripts/NavMeshMoveHelper.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavMeshMoveHelper : MonoBehaviour
+{
+
+    public Transform EndPoint;
+
+    UnityEngine.AI.NavMeshAgent _navMeshAgent;
+    // Use this for initialization
+    void Start()
+    {
+        _navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // if (_navMeshAgent.isOnOffMeshLink)
+        GetComponent<Animator>().SetBool("OffMeshLink", _navMeshAgent.isOnOffMeshLink);
+        _navMeshAgent.SetDestination(EndPoint.position);
+    }
+}
